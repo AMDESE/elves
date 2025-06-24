4503298605ddcc1478e0184ff2c9352780b24f6f
Nikunj A Dadhania <nikunj@amd.com>
2025-06-24 16:15:39 +0530

    Improve create_guest_image.sh
    Improve create_guest_image.sh with dynamic naming and QCOW2 output
    
    - Update Ubuntu 24.04 image URL to stable releases path.
    - Use dynamic filename extraction for downloaded image.
    - Convert to QCOW2 and resize to 10GB, replacing virt-resize.
    - Add growpart and resize2fs for filesystem expansion.
    - Clean up old images and QCOW2 files generically.
    - Refine guest_network.yaml for Ethernet interfaces (e*).
    - Make script executable and update output messages.
    
    Signed-off-by: Nikunj A Dadhania <nikunj@amd.com>
    Signed-off-by: Srikanth Aithal <Srikanth.Aithal@amd.com>

--- create_guest_image.sh
+++ create_guest_image.sh
@@ -43,31 +43,32 @@ if [ ${#missing_bins[@]} -gt 0 ]; then
     detect_os
     install_prereq
 fi
-#Remove any old images by the same name
-rm -f noble-server-cloudimg-amd64.{img,qcow2} 2>/dev/null
+
 #Download Ubuntu 24.04 cloud image (version 20250430, tested)
-image_url="https://cloud-images.ubuntu.com/noble/20250430/noble-server-cloudimg-amd64.img"
-image_file="noble-server-cloudimg-amd64.img"
+image_url="https://cloud-images.ubuntu.com/releases/noble/release-20250430/ubuntu-24.04-server-cloudimg-amd64.img"
+image_file=$(basename $image_url)
+
+#Remove any old images by the same name
+rm -f $image_file ${image_file%.*}*.qcow2 2>/dev/null
+
 wget "$image_url" -O "$image_file" || { echo "Failed to download image"; exit 1; }
 export LIBGUESTFS_BACKEND=direct
-# Create and resize QCOW2 image to 10GB
-qemu-img create -f qcow2 -o preallocation=metadata noble-server-cloudimg-amd64.qcow2 10G || {
-    echo "Failed to create QCOW2 image"; exit 1;
-}
-echo "Expanding the root disk size to 10GB:"
-virt-resize --expand /dev/sda1 noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64.qcow2 || {
-    echo "virt-resize failed"; exit 1;
-}
-# Clean up the original image
-rm -f noble-server-cloudimg-amd64.img 2>/dev/null
+
+mv $image_file ${image_file%.img}.qcow2
+image_file=${image_file%.img}.qcow2
+
+qemu-img resize $image_file 10G
+
 echo "Customizing image with virt-customize:"
-virt-customize -a noble-server-cloudimg-amd64.qcow2 \
+virt-customize -a $image_file \
     --uninstall cloud-init \
     --root-password password:12345678 \
     --install isc-dhcp-client,isc-dhcp-client-ddns,openssh-server,grub2 \
     --copy-in guest_network.yaml:/etc/netplan/ \
     --run-command 'sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config' \
     --run-command 'sed -i "s/#PasswordAuthentication.*/PasswordAuthentication yes/" /etc/ssh/sshd_config' \
+    --run-command 'growpart /dev/sda 1' \
+    --run-command 'resize2fs /dev/sda1' \
     --run-command 'ssh-keygen -A' \
     --run-command 'systemctl enable ssh' \
     --run-command 'grub-install /dev/sda' \
@@ -75,7 +76,7 @@ virt-customize -a noble-server-cloudimg-amd64.qcow2 \
     --delete '/etc/ssh/sshd_config.d/*cloudimg-settings.conf'
 
 if [ $? -eq 0 ]; then
-    echo "Image noble-server-cloudimg-amd64.qcow2 ready to use."
+    echo "Image $image_file ready to use."
 else
     echo "Image creation failed."
     exit 1
